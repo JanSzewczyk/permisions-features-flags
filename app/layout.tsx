81f8bbfa7559c7a89e87ac4c89fda8c94dd7ae15
JanSzewczyk <jan.szewczyk1997@gmail.com>
2025-08-02 13:48:32 +0200

    feat: update ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import * as React from "react";
 
 import { type Metadata } from "next";
 
+import { ClerkProvider, SignedOut, SignInButton } from "@clerk/nextjs";
+import { Header } from "@szum-tech/design-system";
+
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -11,8 +14,22 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="dark">
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <Header>
+            <div className="flex w-full justify-between">
+              <h1 className="text-heading-4">Sandbox App</h1>
+              <div className="ml-16 flex flex-row items-center">
+                <SignedOut>
+                  <SignInButton />
+                </SignedOut>
+              </div>
+            </div>
+          </Header>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
